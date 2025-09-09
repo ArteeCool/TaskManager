edff2477ffa983faa0e3fdd636a87a47a6ed049a
Artem Havryliuk <artem.gawrilyuk@gmail.com>
2025-09-09 10:35:50 +0300

    Fix for email transporter configuration

--- backend/src/controllers/email.controller.ts
+++ backend/src/controllers/email.controller.ts
@@ -2,9 +2,9 @@ import nodemailer from "nodemailer";
 
 export const sendEmail = async (to: string, subject: string, html: string) => {
     const transporter = nodemailer.createTransport({
-        service: "gmail",
-        port: 587,
-        secure: true,
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true, // SSL
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS,
