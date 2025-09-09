61c1fd4d364c5c7bd3e7d7669e74c559a772acb1
Artem Havryliuk <artem.gawrilyuk@gmail.com>
2025-09-09 10:11:40 +0300

    Secure server fix

--- backend/src/controllers/email.controller.ts
+++ backend/src/controllers/email.controller.ts
@@ -4,6 +4,7 @@ export const sendEmail = async (to: string, subject: string, html: string) => {
     const transporter = nodemailer.createTransport({
         service: "gmail",
         port: 587,
+        secure: true,
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS,
