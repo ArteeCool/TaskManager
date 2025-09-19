ec3bec2056a480ceffc2cf1e1f6ec0cd9aed7856
Artem Havryliuk <artem.gawrilyuk@gmail.com>
2025-09-20 01:00:16 +0300

    Added protected route implementation

--- frontend/src/app/routes/ProtectedRoute/ui/ProtectedRoute.tsx
+++ frontend/src/app/routes/ProtectedRoute/ui/ProtectedRoute.tsx
@@ -1,6 +1,40 @@
-import { Outlet } from "react-router";
+// ProtectedRoute.tsx
+import { useContext, useEffect } from "react";
+import { useNavigate, Outlet } from "react-router";
+import { AuthContext } from "@/features/auth/model/context";
+import { useQuery } from "@tanstack/react-query";
+import { useLoader } from "@/features/loader/lib/useLoader";
+import { getCurrentUser } from "@/features/auth/api/getUser";
 
 const ProtectedRoute = () => {
+    const { user } = useContext(AuthContext);
+    const navigate = useNavigate();
+    const { startLoading, finishLoading } = useLoader();
+
+    const { isLoading } = useQuery({
+        queryKey: ["user"],
+        queryFn: getCurrentUser,
+        retry: false,
+        refetchOnWindowFocus: false,
+        refetchOnReconnect: false,
+    });
+
+    useEffect(() => {
+        if (isLoading) {
+            startLoading("auth");
+        } else {
+            finishLoading("auth");
+        }
+    }, [isLoading, startLoading, finishLoading]);
+
+    useEffect(() => {
+        if (!isLoading && !user) {
+            navigate("/login", { replace: true });
+        }
+    }, [isLoading, user, navigate]);
+
+    if (isLoading || !user) return null;
+
     return <Outlet />;
 };
 
